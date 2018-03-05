63386208703fecb2f8f609e61d9464cb758cc1d7
Kiril Nedelev <kiril@collegeswimming.com>
2018-03-05 23:51:15 +0200

    add drop db sql code

--- CreateMigrationTable.sql
+++ CreateMigrationTable.sql
@@ -2,3 +2,10 @@ CREATE TABLE `__EFMigrationsHistory` (
   `MigrationId` text NOT NULL,
   `ProductVersion` text NOT NULL,
   PRIMARY KEY (`MigrationId`(255)));
+
+-- DROP DATABASE
+DROP TABLE `Comment`;
+DROP TABLE `Idea`;
+DROP TABLE `Token`;
+DROP TABLE `User`;
+DELETE FROM `__EFMigrationsHistory`;
