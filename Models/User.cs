cf16e3b4f62ac03b8e9d4fe54d2d53aa406fbac5
Kiril Nedelev <kiril@collegeswimming.com>
2018-03-06 12:14:33 +0200

    change display name

--- Models/User.cs
+++ Models/User.cs
@@ -12,9 +12,11 @@ namespace Ideas.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Required]
